c0e1bca793c48287a043014cd75af948d750d920
unknown <kyoliao@flysheet.com.tw>
2026-07-24 10:49:38 +0800

    feat: add select option for stocktable

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -56,6 +56,8 @@ export function SearchBar({
             <option value={40}>40</option>
             <option value={50}>50</option>
             <option value={60}>60</option>
+            <option value={70}>70</option>
+            <option value={80}>80</option>
           </select>
         </label>
 
